77543fd8617c4eb3024f29fa45b3250e061a32ad
nickschmitt <nickschmitt@gmail.com>
2021-01-18 19:19:27 -0800

    tsunami output to matplotlib

--- tsunami.f90
+++ tsunami.f90
@@ -13,24 +13,43 @@ program tsunami
 
 
     ! # Real (floating point) declarations # !
-    real :: dt ! time step [s]
-    real :: dx ! grid spacing [m]
-    real :: c ! background flow speed [m/s]
+    real, parameter :: dt = 1 ! time step [s]
+    real, parameter :: dx = 1 ! grid spacing [m]
+    real, parameter :: c = 1 ! background flow speed [m/s]
 
     real :: h(grid_size), dh(grid_size) ! arrays for water height and its finite difference.
 
-    ! End Declarations
-
-    ! Initializes reals
-    dt = 1.
-    dx = 1.
-    c = 1.
+    ! Initializing water height with Guassian shape
+    integer, parameter :: icenter = 25
+    real, parameter :: decay = 0.02
     
     ! Checking for values of input parameters
     if (grid_size <= 0) stop 'grid_size must be > 0'
     if (dt <= 0) stop 'time step dt must be > 0'
     if (dx <= 0) stop 'grid spacing dx must be > 0'
     if (c <= 0) stop 'background flow speed c must be > 0'
+
+    ! continuous function using exponential
+    do concurrent(i = 1:grid_size) ! embarrassingly parallel
+        h(i) = exp(-decay * (i - icenter)**2)
+    end do
+
+    print *, 0, h ! initial water height values to terminal
+
+    ! Iterating the solution forward in time
+    time_loop: do n = 1, num_time_steps
+        dh(1) = h(1) - h(grid_size) ! applies periodic boundary condition on the left
+    
+        do concurrent(i = 2: grid_size)
+            dh(i) = h(i) - h(i-1) ! calculates the finite difference of h in space
+        end do
+
+        do concurrent(i = 1: grid_size)
+            h(i) = h(i) - c * dh(i) / dx * dt ! Evaluates h at the next time step
+        end do
+
+        print *, n, h ! Prints current values to terminal
+    end do time_loop
 
     
 end program tsunami
